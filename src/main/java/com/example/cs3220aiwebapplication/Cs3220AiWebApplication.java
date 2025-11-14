deeea7c03a9d413b59804e927d7083ab6d3b7ba5
Edgard Zaragoza <edgardzaragoza719@gmail.com>
2025-11-13 20:28:37 -0800

    edgard

--- src/main/java/com/example/cs3220aiwebapplication/Cs3220AiWebApplication.java
+++ src/main/java/com/example/cs3220aiwebapplication/Cs3220AiWebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cs3220AiWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cs3220AiWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Cs3220AiWebApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/cs3220aiwebapplication/Cs3220AiWebApplicationTests.java
+++ src/test/java/com/example/cs3220aiwebapplication/Cs3220AiWebApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Cs3220AiWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
